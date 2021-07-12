4b7ab63a003e9649bb8f98734886af4db4120d7d
adithyaGowda <adithyagowda5696@gmail.com>
2021-07-12 19:50:13 +0530

    user authentication, register and login, firebase auth

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./components/Header";
 import "./App.css";
 import SideBar from "./components/SideBar";
 import Feed from "./components/Feed";
+import { useSelector, useDispatch } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
+import Login from "./components/Login";
+import { auth } from "./firebase";
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        //user is logged in
+        dispatch(
+          login({
+            email: userAuth.email,
+            uid: userAuth.uid,
+            displayName: userAuth.displayName,
+            photoUrl: userAuth.photoURL,
+          })
+        );
+      } else {
+        //user is logged out
+        dispatch(logout());
+      }
+    });
+  });
   return (
     <div className="app">
       <Header />
-      <div className="app__body">
-        <SideBar />
-        <Feed />
-      </div>
+
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <SideBar />
+          <Feed />
+        </div>
+      )}
     </div>
   );
 }
